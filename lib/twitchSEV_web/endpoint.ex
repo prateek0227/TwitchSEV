8a257e16aa337a299ecdb8b2cb70ff567de8b8de
prateek0227 <prateekgarcha@gmail.com>
2019-03-13 21:40:10 +0530

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/twitchSEV_web/endpoint.ex
+++ lib/twitchSEV_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule TwitchSEVWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :twitchSEV
 
   socket "/socket", TwitchSEVWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
